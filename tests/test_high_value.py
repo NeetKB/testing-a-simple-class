5cfc931e96f2e6d48438a243bbb3d4243306c878
Avnita <akbhandal@gmail.com>
2024-08-06 15:24:29 +0100

    Added six more tests

--- tests/test_high_value.py
+++ tests/test_high_value.py
@@ -9,3 +9,31 @@ def test_get_highest_first_higher():
     val = HighValue(10,3)
     result = val.get_highest()
     assert result == "First value is higher"
+
+def test_get_highest_add_seven_first():
+    val = HighValue(0,0)
+    val.add(7,"first")
+    assert val.value_first == 7
+
+def test_get_highest_second_higher():
+    val = HighValue(2,6)
+    result = val.get_highest()
+    assert result == "Second value is higher"
+
+def test_get_highest_add_and_check():
+    val = HighValue(3,4)
+    val.add(3, "first")
+    assert val.get_highest() == "First value is higher"
+
+def test_get_highest_second_attribute():
+    val = HighValue(5,10)
+    assert val.value_second == 10
+
+def test_get_highest_first_attribute():
+    val = HighValue(14,5)
+    assert val.value_first == 14
+
+def test_get_highest_add_negative():
+    val = HighValue(6,11)
+    val.add(-12, "second")
+    assert val.get_highest() == "First value is higher"
